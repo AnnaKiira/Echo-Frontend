599e33be6bedcc6595cb936095381602a665fd62
AnnaKiira <annakirath@gmail.com>
2026-03-28 23:08:59 +0100

    removed consolelog

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ function App() {
   const  [healthData, setHealthData] = useState(null)
 
   useEffect(() => {
-    console.log(import.meta.env.VITE_BACKEND_URL)
     fetch(`${import.meta.env.VITE_BACKEND_URL}/health`)
       .then(response => response.json())
       .then(data => setHealthData(data))

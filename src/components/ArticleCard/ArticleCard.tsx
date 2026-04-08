e49b3b9d8547b93b2b3b773436b55a0d373e25d1
AnnaKiira <annakirath@gmail.com>
2026-04-08 22:50:15 +0200

    starting to style the news cards

--- src/components/ArticleCard/ArticleCard.tsx
+++ src/components/ArticleCard/ArticleCard.tsx
@@ -1,13 +1,14 @@
 import type { Article } from '../../types'
+import styles from './ArticleCard.module.css'
 
 const ArticleCard = ({article}: {article: Article}) => {
     return (
-            <article>
+            <article className={styles.newsCard}>
 
                 <header>
-                    {article.urlToImage ? <img src={article.urlToImage} alt={article.title} /> : null}
-                    <h2>{article.title}</h2>
-                    <span> 
+                    {article.urlToImage ? <img className={styles.newsImage} src={article.urlToImage} alt={article.title} /> : null}
+                    <h3 className={styles.newsTitle}>{article.title}</h3>
+                    <span className={styles.newsSource}> 
                         {article.source.name ? article.source.name : "Unknown Source"}
                     </span>
 
